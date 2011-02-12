8e0ddeaf86f6b7d45eb8b307eb8090669704e3c8
David Collien <davidcollien@gmail.com>
2011-02-12 09:50:13 +0000

    The one thing you wish i'd forget Is saying goodbye, saying goodbye Ooh, Goodbye
    ... I wish i could svn rm miley cirus

--- ide/src/app/plugin/interfaces/IMenuBar.java
+++
@@ -1,7 +0,0 @@
-package app.plugin.interfaces;
-
-import org.eclipse.swt.widgets.Menu;
-
-public interface IMenuBar {
-	Menu addNewMenu( String menuText );
-}
--- ide/src/plugins/toolTabs/serial/SerialTerminalPlugin.java
+++
@@ -1,5 +0,0 @@
-package plugins.toolTabs.serial;
-
-public class SerialTerminalPlugin {
-
-}
